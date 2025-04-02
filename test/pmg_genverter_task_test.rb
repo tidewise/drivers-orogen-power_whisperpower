1e609e8fcd1b182926cf0c38462f80fa69ca3ebd
eduardacoppo <eduarda.coppo@tidewise.io>
2025-04-02 11:53:19 -0300

    test: add tests for control_cmd

--- test/pmg_genverter_task_test.rb
+++ test/pmg_genverter_task_test.rb
@@ -78,6 +78,57 @@ describe OroGen.power_whisperpower.PMGGenverterTask do
         end.to { have_no_new_sample task.full_status_port }
     end
 
+    it "does not send a command if one of the ready to command messages " \
+       "were not received (messages with can_id 0x204, 0x205)" do
+        expect_execution do
+            syskit_write task.control_cmd_port, true
+        end.to { have_no_new_sample(task.can_out_port, at_least_during: 1) }
+
+        expect_execution.to do
+            have_no_new_sample(task.can_out_port, at_least_during: 1)
+        end
+    end
+
+    it "sends a NO COMMAND message for 30ms before sending the command" do
+        expect_execution do
+            syskit_write(
+                task.can_in_port,
+                make_read_reply(0x205)
+            )
+        end.to { have_one_new_sample task.full_status_port }
+
+        no_command = expect_execution do
+            syskit_write task.control_cmd_port, true
+        end.to do
+            have_one_new_sample(task.can_out_port)
+                .matching { |s| no_command(s) }
+        end
+
+        command = expect_execution.to do
+            have_one_new_sample(task.can_out_port)
+                .matching { |s| enable_generation(s) }
+        end
+
+        assert no_command.time <= command.time
+        assert_in_delta(
+            (command.time - no_command.time),
+            0.03,
+            1e-2
+        )
+    end
+
+    def enable_generation(sample)
+        sample.data[0] == 1 && sample.data[1] == 0
+    end
+
+    def disable_generation(sample)
+        sample.data[0] == 0 && sample.data[1] == 1
+    end
+
+    def no_command(sample)
+        sample.data[0] == 0 && sample.data[1] == 0
+    end
+
     def rock_now
         now = Time.now
         Time.at(now.tv_sec, now.tv_usec / 1000, :millisecond)
